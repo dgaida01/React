dda6e918ae3926a8d91a43fe809e20f809149dc5
dgaida01 <dean.gaida@hotmail.com>
2022-01-13 22:00:43 -0500

    additional formatting

--- 04-function-form/src/App.js
+++ 04-function-form/src/App.js
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App container ms-5">
       <div class="row">
-        <div class="col">Column1</div>
+        <div class="col"></div>
         <div class="col-5 text-center "  >
         <HookForm></HookForm>
         </div>
-        <div class="col">Column3</div>
+        <div class="col"></div>
       </div>
     </div>
   );
