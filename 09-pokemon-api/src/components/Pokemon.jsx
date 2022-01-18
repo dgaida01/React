38e1172039465044f682dbbffc28d193bad5d033
dgaida01 <dean.gaida@hotmail.com>
2022-01-18 15:36:52 -0500

    Pokemon with Fetch

---
+++ 09-pokemon-api/src/components/Pokemon.jsx
@@ -0,0 +1,39 @@
+import React,{useState} from 'react'
+
+let Pokemon = () =>{
+
+    let [listOfPokemon,setListOfPokemon]=useState([])
+
+    let getPokemon =()=>{
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=800")
+        .then(response=>{
+            return response.json();
+        })
+        .then(response=>{
+            setListOfPokemon(response.results);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+
+    return(
+        <>
+        <h1>Pokemon Component</h1>
+        <button onClick={getPokemon} >Fetch Pokemon</button>
+        {
+            listOfPokemon.map((pokeObj,idx)=>{
+                return( 
+                    <ul key={idx} className='d-flex  justify-content-center'>
+                    <li className='w-25 p-2 text-start'>{pokeObj.name}</li>
+                    </ul>
+                )}  
+            )
+        }
+   
+    
+        </>
+    )
+}
+
+export default Pokemon;
